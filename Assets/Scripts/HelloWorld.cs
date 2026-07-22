1dc4f5cfe68aaea05572fba4b927f42f86b828cc
walthe219 <walthe219@gmail.com>
2026-07-22 18:53:43 -0500

    Created 3D project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("HelloWorld"); 
+    }
+}
